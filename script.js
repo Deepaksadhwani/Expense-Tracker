f1df50dbfa0ef3af5db883d725a44c610a7d649e
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2023-12-11 02:10:46 +0530

    Added delete and edit functionality

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ function displayExpense(userData){
     const ulist = document.getElementById("newExpense");
     const li =  document.createElement("li"); 
     li.className = "expenselist";
-
+    li.setAttribute('data-user-data', JSON.stringify(userData));
     const text = document.createTextNode("Expenseamount: " + userData.expenseAmountValue +
-    " discription: " + userData.discriptionValue + " Category: " + userData.categoryValue);
+    " discription: " + userData.discriptionValue + " Category: " + userData.categoryValue + " ");
 
     const deleteButton = document.createElement("button");
     deleteButton.classList = "delete";
@@ -47,3 +47,49 @@ function displayExpense(userData){
     
     ulist.appendChild(li);
     }
+
+const ulist = document.getElementById("newExpense");
+
+ulist.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("delete")){
+        const li = e.target.parentElement;
+        const  userStorageData = JSON.parse(li.getAttribute('data-user-data'));
+        localStorage.removeItem(userStorageData.id);
+        li.remove();
+    }
+    
+    if (e.target.classList.contains("edit")){
+        const liEdit = e.target.parentElement;
+        const userEditData = JSON.parse(liEdit.getAttribute("data-user-data"));
+
+        let expenseAmount = document.getElementById("amount");
+        let discription  = document.getElementById("detail");
+        let category = document.getElementById("category");
+
+        expenseAmount.value = userEditData.expenseAmountValue;
+        discription.value = userEditData.discriptionValue;
+        category.value = userEditData.categoryValue;
+
+        localStorage.removeItem(userEditData.id);
+        liEdit.remove();
+
+    }
+})
+
+function displayExistingDisplay(){
+    for(let i =0; i < localStorage.length;i++){
+        const key = localStorage.key(i);
+        const userExsitingData = JSON.parse(localStorage.getItem(key));
+        displayExpense(userExsitingData);
+    }
+    if (localStorage.length === 0){
+        count = 1;
+    }
+    else{
+        count++
+    }
+}
+
+window.addEventListener("load", ()=>{
+    displayExistingDisplay();
+})
